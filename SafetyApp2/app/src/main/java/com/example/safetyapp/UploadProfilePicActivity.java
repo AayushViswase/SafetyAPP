404fdb5d37533a963b7e40e0c569b2833737f253
AayushViswase <aayushviswase10@gmail.com>
2023-03-24 01:01:57 +0530

    ForgotPasswordActivity and UpdateProfileActivity Done

---
+++ SafetyApp2/app/src/main/java/com/example/safetyapp/UploadProfilePicActivity.java
@@ -0,0 +1,190 @@
+package com.example.safetyapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.net.URI;
+
+public class UploadProfilePicActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private ImageView imageViewUploadPic;
+    private FirebaseAuth authProfile;
+    private StorageReference storageReference;
+    private FirebaseUser firebaseUser;
+    private static final int PICK_IMAGE_REQUEST=1;
+    private Uri uriImage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_profile_pic);
+        setContentView(R.layout.activity_upload_profile_pic);
+        getSupportActionBar().setTitle("Upload Profile Picture");
+
+        Button buttonUploadPicChoose=findViewById(R.id.upload_pic_choose_button);
+        Button buttonUploadPic=findViewById(R.id.upload_pic_button);
+        imageViewUploadPic=findViewById(R.id.imageView_profile_dp);
+        authProfile=FirebaseAuth.getInstance();
+        firebaseUser=authProfile.getCurrentUser();
+
+        storageReference= FirebaseStorage.getInstance().getReference("DisplayPics");
+        Uri uri=firebaseUser.getPhotoUrl();
+
+        //Set Users current DP in ImageVIew(if uplaoded already).We will pocasso since setImage
+        //Regular URIs
+        Picasso.with(UploadProfilePicActivity.this).load(uri).into(imageViewUploadPic);
+
+        //Chooseing image to upload
+        buttonUploadPicChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+
+        //Upload Image
+        buttonUploadPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                UploadPic();
+            }
+        });
+
+
+
+
+    }
+
+    private void UploadPic() {
+        if(uriImage!=null){
+            //save the image with uid pf thr currently logged user
+            StorageReference fileRefrence=storageReference.child(authProfile.getCurrentUser().getUid()+"."+getFIleExtention(uriImage));
+
+            //Upload image to storage
+            fileRefrence.putFile(uriImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    fileRefrence.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Uri downloadUri=uri;
+                            firebaseUser=authProfile.getCurrentUser();
+
+                            //finally set the display image of the user after uplaod
+                            UserProfileChangeRequest profileUpdate=new  UserProfileChangeRequest.Builder().setPhotoUri(downloadUri).build();
+                            firebaseUser.updateProfile(profileUpdate);
+                        }
+                    });
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(UploadProfilePicActivity.this, "Upload Successfull", Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(UploadProfilePicActivity.this,UserProfileActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(UploadProfilePicActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(UploadProfilePicActivity.this, "No file Selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getFIleExtention(Uri uri) {
+        ContentResolver cR=getContentResolver();
+        MimeTypeMap mime=MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void openFileChooser() {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==PICK_IMAGE_REQUEST && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+            uriImage=data.getData();
+            imageViewUploadPic.setImageURI(uriImage);
+        }
+    }
+
+    //creatinf action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.common_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.menu_refresh){
+            startActivity(getIntent());
+            finish();
+            overridePendingTransition(0,0);
+        } else if (id==R.id.menu_update_profile) {
+            Intent intent=new Intent(UploadProfilePicActivity.this,UpdateProfileActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else if (id==R.id.menu_update_email) {
+            Intent intent = new Intent(UploadProfilePicActivity.this, UpdateProfileActivity.class);
+            startActivity(intent);
+        }else if (id==R.id.menu_change_password) {
+            Intent intent = new Intent(UploadProfilePicActivity.this, ChangePasswordActivity.class);
+            startActivity(intent);
+        }else if (id==R.id.menu_delete_profile) {
+            Intent intent = new Intent(UploadProfilePicActivity.this, DeleteProfileActivity.class);
+            startActivity(intent);
+        }else if (id==R.id.menu_logout) {
+            authProfile.signOut();
+            Toast.makeText(UploadProfilePicActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(UploadProfilePicActivity.this,MainActivity.class);
+            //Clear stack tpo prevent user comming back to userProfile Activityon back button
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        }else {
+
+            Toast.makeText(UploadProfilePicActivity.this, "Somenthing went Wrong", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- SafetyApp2/app/src/main/java/com/example/safetyapp/UserProfileActivity.java
+++ SafetyApp2/app/src/main/java/com/example/safetyapp/UserProfileActivity.java
@@ -1,9 +1,15 @@
 package com.example.safetyapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -17,6 +23,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -41,6 +48,16 @@ public class UserProfileActivity extends AppCompatActivity {
         textViewGender = findViewById(R.id.textView_show_gender);
         textViewMobile = findViewById(R.id.textView_show_mobile);
         progressBar = findViewById(R.id.progress_bar);
+        //Set onclicklistner on Inmage to open Upload Profile pic
+        imageView=findViewById(R.id.imageView_profile_dp);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(UserProfileActivity.this,UploadProfilePicActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
         authProfile = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = authProfile.getCurrentUser();
@@ -49,10 +66,37 @@ public class UserProfileActivity extends AppCompatActivity {
         if (firebaseUser == null) {
             Toast.makeText(UserProfileActivity.this, "Something went wrong ! User's details are not available at the moment", Toast.LENGTH_LONG).show();
         } else {
+            checkidEmailVerified(firebaseUser);
             progressBar.setVisibility(View.VISIBLE);
             showUserProfile(firebaseUser);
 
         }
+    }
+
+    private void checkidEmailVerified(FirebaseUser firebaseUser) {
+        if (!firebaseUser.isEmailVerified()){
+            showAlertDialog();
+
+        }
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder=new AlertDialog.Builder(UserProfileActivity.this);
+        builder.setTitle("Email Not Verified");
+        builder.setMessage("Please Verify you Email now.You can not Login without email verification.");
+
+        //Open email Apps if User clicks continue button
+        builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent=new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        AlertDialog alertDialog= builder.create();
+        alertDialog.show();
     }
 
     private void showUserProfile(FirebaseUser firebaseUser){
@@ -78,7 +122,16 @@ public class UserProfileActivity extends AppCompatActivity {
                     textViewDoB.setText(doB);
                     textViewGender.setText(gender);
                     textViewMobile.setText(mobile);
+                    
+                    //Set user Dp
+                    Uri uri=firebaseUser.getPhotoUrl();
+                    
+                    //imageView setImageUR()  using picaaso
+                    Picasso.with(UserProfileActivity.this).load(uri).into(imageView);
+                    
 
+                }else {
+                    Toast.makeText(UserProfileActivity.this, "Something went Wrong", Toast.LENGTH_SHORT).show();
                 }
                 progressBar.setVisibility(View.GONE);
             }
@@ -92,6 +145,47 @@ public class UserProfileActivity extends AppCompatActivity {
             }
         });
     }
+//creatinf action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.common_menu,menu);
 
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.menu_refresh){
+            startActivity(getIntent());
+            finish();
+            overridePendingTransition(0,0);
+        } else if (id==R.id.menu_update_profile) {
+            Intent intent=new Intent(UserProfileActivity.this,UpdateProfileActivity.class);
+            startActivity(intent);
+        }
+        else if (id==R.id.menu_update_email) {
+            Intent intent = new Intent(UserProfileActivity.this, UpdateProfileActivity.class);
+            startActivity(intent);
+        }else if (id==R.id.menu_change_password) {
+            Intent intent = new Intent(UserProfileActivity.this, ChangePasswordActivity.class);
+            startActivity(intent);
+        }else if (id==R.id.menu_delete_profile) {
+            Intent intent = new Intent(UserProfileActivity.this, DeleteProfileActivity.class);
+            startActivity(intent);
+        }else if (id==R.id.menu_logout) {
+            authProfile.signOut();
+            Toast.makeText(UserProfileActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(UserProfileActivity.this,MainActivity.class);
+            //Clear stack tpo prevent user comming back to userProfile Activityon back button
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        }else {
+
+            Toast.makeText(UserProfileActivity.this, "Somenthing went Wrong", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
