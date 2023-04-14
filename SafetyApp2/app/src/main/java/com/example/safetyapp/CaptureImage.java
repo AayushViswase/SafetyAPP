d29382ce47f819406e9b6c60a0b0b820b1e6a6fb
AayushViswase <aayushviswase10@gmail.com>
2023-04-14 23:02:43 +0530

    image capturing

--- SafetyApp2/app/src/main/java/com/example/safetyapp/CaptureImage.java
+++ SafetyApp2/app/src/main/java/com/example/safetyapp/CaptureImage.java
@@ -1,26 +1,35 @@
 package com.example.safetyapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.security.Permission;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CaptureImage extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_CAMERA_PERMISSION = 1;
     private ImageView imageView;
+    private File photoFile;
+    private Uri photoUri;
     private Button captureButton;
 
     @Override
@@ -29,19 +38,50 @@ public class CaptureImage extends AppCompatActivity {
         setContentView(R.layout.activity_capture_image);
         imageView = findViewById(R.id.imageView);
         captureButton = findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                requestCameraPermission(); // call requestCameraPermission() when the capture button is clicked
-            }
+        captureButton.setOnClickListener(v -> {
+            requestCameraPermission(); // call requestCameraPermission() when the capture button is clicked
         });
     }
 
     private void captureImage() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, "My Image");
+            values.put(MediaStore.Images.Media.DESCRIPTION, "Image Captured By Camera");
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+
+            // Save the image to the MediaStore
+            ContentResolver resolver = getContentResolver();
+            photoUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+            // Save the captured image to the MediaStore
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Show the captured image in the ImageView
+            imageView.setImageURI(photoUri);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create a unique filename for the captured image
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        return image;
     }
 
     private void requestCameraPermission() {
@@ -68,13 +108,5 @@ public class CaptureImage extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
-    }
+
 }
